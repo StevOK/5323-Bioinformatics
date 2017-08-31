44efeae4375970343cf36eafdc7b4da62d10a065
Steven Kundert <stevenkundert@yahoo.com>
2017-08-30 21:22:42 -0500

    Update Steven Kundert - Fibonacci Warmup.py

--- Steven Kundert - Fibonacci Warmup.py
+++ Steven Kundert - Fibonacci Warmup.py
@@ -7,14 +7,24 @@
 # a file of numbers and prints each number's corresponding 
 # Fibonacci number.
 
+#Create list, fill in its first two entries
 fib = [0, 1]
 
-for n in range(2, 5000):
+#Generate rest of list
+for n in range(2, 5001):
 	fib.append(fib[n-1] + fib[n-2])
 	
+#Open input file for reading	
 file = open('fibdata.txt', 'r')
+
+#Read the first line
 line = file.readline()
+
+#While there are still lines in the input file
 while line:
+	#Convert each line into an integer
 	k = int(line)
+	#Print the number and its corresponding Fib number
 	print('The Fibonacci number for', k, 'is', fib[k])
+	#Read in the next line
 	line = file.readline()
