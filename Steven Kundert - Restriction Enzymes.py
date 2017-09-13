f57a5d6d8451533996c729501087cadd56cce8da
Steven Kundert <stevenkundert@yahoo.com>
2017-09-13 01:13:28 -0500

    Update Steven Kundert - Restriction Enzymes.py

--- Steven Kundert - Restriction Enzymes.py
+++ Steven Kundert - Restriction Enzymes.py
@@ -2,30 +2,60 @@
 # CMPS 5323 - Simpson
 # HW 2 - Restriction Enzymes
 # 9/13/17
-#
-#
-#
+# This program simulates the cutting up of a DNA strand by the HaeIII 
+#  restriction enzyme. A sample DNA sequence is sent into a function
+#  where the palindromic subsequence GGCC will be searched for. When
+#  the function finds the sequence, it "splits" the strand by creating
+#  a new string from the most recent split (or the beginning) to the
+#  middle of GGCC. It will continue until the entire strand has been
+#  split up and return the list of strings, which are then printed,
+#  along with their associated complementary strands.
 
-# Take a DNA sequence, return a list of substrings
+# Function: HaeIII()
+# Input: One string, representing a DNA sequence
+# Process: Use the find() method to search for each occurrence of
+#			the subsequence GGCC, then append() each subsequence
+#			between these endpoints. 
+# Output: A list of strings
+# Take a DNA sequence, return a list of substrings. This should be 
+#  O(n) complexity because of the str.find() calls.
 def HaeIII(dna):
-	lst = []
-	str1 = 'GGCC'
-	start = 0
-	while start <= (len(dna) - 1):
-		end = (dna.find(str1, start)) + 2
-		if end >= len(dna):
-			end = len(dna) - 1
-		lst.append(dna[start:end])
-		start = end
-	return lst
+    lst = []        # Create a list to put substrings into
+    str1 = 'GGCC'   # We will be looking for this subsequence
+    start = 0       # Start at the beginning of the string
+    
+    # While the search hasn't reached the end of the sequence 
+    while start < len(dna):
+        # Put a splitting point between 'GG' and 'CC'
+		# The str.find() call returns -1 if it reaches the end
+		#  of the string, so we will get end == 1 then 
+        end = dna.find(str1, start) + 2
+        # If the end of the string has been reached,
+        #  put the endpoint at the end of the sequence.
+        if end == 1:
+            end = len(dna)
+            
+        # Append a substring from the sequence into the list
+        lst.append(dna[start:end])
+        
+        # Put the new start point at the most recent end point
+        start = end
+       
+    return lst
 		
 		
-	
+# Create a sequence to be cut up
 sequence = 'ACGAACATCTTTGGCCAACTAGACCTGGCCAACCTAGCGG'	
+
+# Send sequence to function, store list of strings
 subseqs = HaeIII(sequence)
+
+# Create the translation table to mirror each substring
 intab = 'ACGT'
 outtab = 'TGCA'
 trantab = str.maketrans(intab, outtab)
+
+# Print each substring and its complement
 for i in subseqs:
 	print(i)
 	print(i.translate(trantab), "\n")
